fd56a0963edd680a4c4dc9f8f1f238b5290617fb
Doyle Huang <doyle.sy.huang@mail.foxconn.com>
2017-08-21 14:01:41 +0800

    Revert "Revert "feature: kick watchdog for redfish, hwmon, event service""
    This reverts commit 4e86a5bbe2d0890a1be5fe743624f64074cfdeda.
    
    Change-Id: I3b34cf5e8466ff4d5258ec1431c783fee07f0b15
    Signed-off-by: Doyle Huang <doyle.sy.huang@mail.foxconn.com>

--- meta-openbmc-machines/meta-microsoft/meta-g50/recipes-phosphor/obmc-ast-watchdog/files/obmc-ast-watchdog.c
+++ meta-openbmc-machines/meta-microsoft/meta-g50/recipes-phosphor/obmc-ast-watchdog/files/obmc-ast-watchdog.c
@@ -12,8 +12,28 @@
 
 #define WATCHDOG_MODULE		"/dev/watchdog"
 
+
+#define WATCH_MISS_THRESHOLD 5
+
+enum{
+    WATCH_REDFISH,
+    WATCH_HWMON,
+    WATCH_EVENT_SERVICE,
+    WATCH_COUNT
+};
+
+const char *WATCH_FILE_PATH[] = {
+    [WATCH_REDFISH]        = "/var/lib/obmc/watch_redfish",
+    [WATCH_HWMON]          = "/var/lib/obmc/watch_hwmon",
+    [WATCH_EVENT_SERVICE]  = "/var/lib/obmc/watch_event_service",
+};
+
 void main()
 {
+    static char watch[WATCH_COUNT] = {0};
+    char watchMissCount = 0;
+    int i, c = 0;
+    FILE *fp = NULL;
 	int fd = open(WATCHDOG_MODULE, O_RDWR);
 	
 	printf("\nStart Watchdog ... \n");	
@@ -26,12 +46,31 @@ void main()
 	
 	while(1)
 	{
-		//default hardware watchdog timeout is 30 seconds
-		if(ioctl(fd, WDIOC_KEEPALIVE) < 0)
-			printf("WDIOC_KEEPALIVE IOCTL error!\n");
-				
+        watchMissCount = 0;
+        for(i=0; i<WATCH_COUNT; ++i){
+            fp = fopen(WATCH_FILE_PATH[i], "r");
+            if(fp == NULL){
+                watch[i] = 0;
+                fp = fopen(WATCH_FILE_PATH[i], "w+");
+            }else{
+                if(watch[i] < WATCH_MISS_THRESHOLD){
+                    watch[i]++;
+                }else{
+                    watchMissCount++;
+                }
+                printf("@@@ check %s exist, watch[%d] = %d, watchMissCount = %d\n", WATCH_FILE_PATH[i], i, watch[i], watchMissCount);
+            }
+            fclose(fp);
+        }
+
+        if(watchMissCount == 0){
+            //default hardware watchdog timeout is 30 seconds
+            if(ioctl(fd, WDIOC_KEEPALIVE) < 0){
+                printf("WDIOC_KEEPALIVE IOCTL error!\n");
+            }
+        }
 		sleep(5);		
 	}
-	
+
 	return;
 }
