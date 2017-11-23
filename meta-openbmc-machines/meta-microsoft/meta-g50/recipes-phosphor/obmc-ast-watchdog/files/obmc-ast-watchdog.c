189bd67e163ecd0280f11f88aa1e88123df79947
Wei-Chung Wen <wei-chung.wen@foxconn.com>
2017-11-23 10:42:40 +0800

    obmc-ast-watchdog: rewrite to support both heartbeat and PID files
    In the case of heartbeat file, this program will periodically recreate
    heartbeat file, and service is expected to delete this file to prove its
    responsiveness.
    
    In the case of PID file, service is considered responsive as long as
    /proc/PID exists.
    
    A service will be considered dead if it is non-responsive more than 10
    successive checks.
    
    This program checks every 5 seconds whether all services (it cares) are
    alive. Only when all services are alive will WDT1 be kicked.
    
    Currently, 3 services are checked:
    1. Redfish
    2. Hwmon
    3. Event daemon
    
    Change-Id: Iae68caf8b7090cb1aeb92bca5731ddf00889f611
    Signed-off-by: Wei-Chung Wen <wei-chung.wen@foxconn.com>

--- meta-openbmc-machines/meta-microsoft/meta-g50/recipes-phosphor/obmc-ast-watchdog/files/obmc-ast-watchdog.c
+++ meta-openbmc-machines/meta-microsoft/meta-g50/recipes-phosphor/obmc-ast-watchdog/files/obmc-ast-watchdog.c
@@ -4,150 +4,199 @@
 *                                                            *
 *************************************************************/
 
+#include <errno.h>
+#include <fcntl.h>
+#include <linux/watchdog.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
-#include <linux/watchdog.h>
+#include <time.h>
+#include <unistd.h>
 
-#define WATCHDOG_MODULE     "/dev/watchdog"
+#define MAX_NONRESPONSE 10
 
-#define WATCH_MISS_THRESHOLD  (10)
-#define MAX_LEN_FILE_NAME (50)
+struct Service {
+    /* Check service by heartbeat file.
+     *
+     * Hearbeat file is periodically recreated by this program. Service is
+     * expected to delete this file to prove its responsiveness.
+     *
+     * This can be NULL. */
+    const char *heartbeat;
 
-enum em_watch_type{
-    EM_WATCH_REDFISH = 0,
-    EM_WATCH_HWMON,
-    EM_WATCH_EVENT_SERVICE,
-    EM_WATCH_COUNT
+    /* Check service by PID file.
+     *
+     * Service will be considered responsive as long as /proc/PID exists.
+     *
+     * This can be NULL. */
+    const char *pidfile;
+
+    /* How many times heartbeat test or pidfile test fails.
+     *
+     * Should this be larger than MAX_NONRESPONSE, this service is considered
+     * dead. */
+    uint32_t nNonresponse;
 };
 
-struct st_watch_record {
-    enum em_watch_type type;
-    char file_path[MAX_LEN_FILE_NAME];
-    unsigned int miss_count;
+static struct Service Services[] = {
+    { "/run/obmc/watch_redfish", NULL, 0 },
+    { "/run/obmc/watch_hwmon", NULL, 0 },
+    { NULL, "/run/obmc-phosphor-event.pid", 0 },
 };
 
-struct st_watch_record  g_watch_tab[EM_WATCH_COUNT] = {
-    {EM_WATCH_REDFISH, "/run/obmc/watch_redfish", 0},
-    {EM_WATCH_HWMON, "/run/obmc/watch_hwmon", 0},
-    {EM_WATCH_EVENT_SERVICE, "/run/obmc/watch_event_service", 0},
-};
+static const int SERVICE_COUNT = sizeof(Services) / sizeof(struct Service);
 
-static void create_new_watch_file(char *path)
-{
-    FILE *fp = NULL;
-    if (path == NULL)
-        return;
-    fp = fopen(path, "w+");
-    if (fp != NULL)
-        fclose(fp);
-}
-
-//@return: 1-success to check watch file; 0-it is abnormal to check watch file
-static int check_watch_file_status()
-{
-    int i; 
-    for (i = 0; i<EM_WATCH_COUNT; i++)
-    {
-        if( access( g_watch_tab[i].file_path, F_OK ) == -1 )
-        {
-            g_watch_tab[i].miss_count = 0;
-            create_new_watch_file( g_watch_tab[i].file_path);
-        }
-        else
-        {
-            g_watch_tab[i].miss_count+=1;
-            if(g_watch_tab[i].miss_count > WATCH_MISS_THRESHOLD)
-                return 0;
-        }
-    }
-    return 1;
-}
-
-//@return: 1-success for system stable; 0-it is abnormal for system stable
-static int wait_system_stable()
-{
-    int i;
-
-    //create all watch file
-    for (i = 0; i<EM_WATCH_COUNT; i++)
-        create_new_watch_file(g_watch_tab[i].file_path);
-
-    //wait and check all task is alive to delete relative watch file
-    while (1)
-    {
-        for (i = 0; i<EM_WATCH_COUNT; i++)
-            if( access( g_watch_tab[i].file_path, F_OK ) != -1 )
-                break;
-        //check all releate watch files are deleted
-        if (i == EM_WATCH_COUNT)
-            return 1;
-        sleep(5);
-    }
-    return 0;
-}
-
-static void save_pid (void) {
+static void save_pid(void) {
     pid_t pid = 0;
     FILE *pidfile = NULL;
     pid = getpid();
     if (!(pidfile = fopen("/run/obmc-ast-watchdog.pid", "w"))) {
-        fprintf(stderr, "failed to open pidfile\n");
+        fprintf(stderr, "ERROR on fopen(/run/obmc-ast-watchdog.pid)\n");
         return;
     }
     fprintf(pidfile, "%d\n", pid);
     fclose(pidfile);
 }
 
-void WDT2_disable()
-{
-    system("devmem 0x1e78502c 32 0");
-    return;
+static void create_heartbeat_file(const struct Service *srv) {
+    FILE *fp = NULL;
+
+    if (srv->heartbeat) {
+        if (!(fp = fopen(srv->heartbeat, "w+"))) {
+            fprintf(stderr, "ERROR on fopen(%s): %s\n",
+                    srv->heartbeat, strerror(errno));
+            return;
+        }
+        fclose(fp);
+    }
 }
 
-void main()
-{
-    struct stat st = {0};
-    int fd = -1;
+static int is_responsive(const struct Service *srv) {
+    int responsive = 1;
+    FILE *fp = NULL;
+    int pid = 0;
+    char path[64] = {0};
 
-    save_pid();
-
-    //check and create /run/obmc/ directory
-    if (stat("/run/obmc/", &st) == -1) {
-        mkdir("run/obmc/", 0755);
+    if (responsive && srv->heartbeat) {
+        responsive = (access(srv->heartbeat, F_OK) == -1);
     }
 
-    if (wait_system_stable() == 0)
-    {
-        printf("\nDisable Watchdog for system stable timeout !!\n");  
-        return;
+    if (responsive && srv->pidfile) {
+        if ((fp = fopen(srv->pidfile, "r"))) {
+            if (fscanf(fp, "%d", &pid) != 1) {
+                responsive = 0;
+            }
+            fclose(fp);
+
+            if (responsive) {
+                if (snprintf(path, 64, "/proc/%d", pid) < 64) {
+                    responsive = !access(path, F_OK);
+                } else {
+                    fprintf(stderr, "ERROR not enough space for process path\n");
+                    responsive = 0;
+                }
+            }
+        } else {
+            responsive = 0;
+        }
     }
 
-    fd = open(WATCHDOG_MODULE, O_RDWR);
-    if(fd < 0)
-    {
-        printf("open '%s' error!\n", WATCHDOG_MODULE);
-        return;
+    return responsive;
+}
+
+static void wait_until_all_services_responsive(void) {
+    int i;
+
+    for (i = 0; i < SERVICE_COUNT; i++) {
+        create_heartbeat_file(&Services[i]);
     }
 
-    //After wait system stable and enabled watchdog1, watchdog2 can be disabled
-    WDT2_disable();
+    for (i = 0; i < SERVICE_COUNT; i++) {
+        while (!is_responsive(&Services[i])) {
+            sleep(5);
+        }
+    }
+}
 
-    printf("\nStart Watchdog ... \n");  
-    while(1)
-    {
-        if(check_watch_file_status() == 1){
-            //default hardware watchdog timeout is 30 seconds
-            if(ioctl(fd, WDIOC_KEEPALIVE) < 0){
-                printf("WDIOC_KEEPALIVE IOCTL error!\n");
+static int enable_WDT1(int *wdt1) {
+    int fd = open("/dev/watchdog", O_RDWR);
+
+    if (fd != -1) {
+        *wdt1 = fd;
+        return 0;
+    } else {
+        fprintf(stderr, "ERROR on open(/dev/watchdog): %s\n", strerror(errno));
+        return -1;
+    }
+}
+
+static void disable_WDT2(void) {
+    system("devmem 0x1e78502c 32 0");
+}
+
+static int all_services_alive(void) {
+    int alive = 1;
+    int i;
+
+    for (i = 0; i < SERVICE_COUNT; i++) {
+        if (is_responsive(&Services[i])) {
+            Services[i].nNonresponse = 0;
+            create_heartbeat_file(&Services[i]);
+        } else {
+            Services[i].nNonresponse++;
+            if (Services[i].nNonresponse > MAX_NONRESPONSE) {
+                alive = 0;
             }
         }
-        sleep(5);       
     }
-    close(fd);
-    return;
+
+    return alive;
+}
+
+static void kick_WDT1(int wdt1) {
+    if (ioctl(wdt1, WDIOC_KEEPALIVE)) {
+        fprintf(stderr, "ERROR on ioctl(wdt1, WDIOC_KEEPALIVE): %s\n",
+                strerror(errno));
+    }
+}
+
+static void disable_WDT1(int wdt1) {
+    close(wdt1);
+}
+
+int main(int argc, char** argv) {
+    int wdt1 = -1;
+
+    /* Save PID to pre-defined PID file so that BMC Health sensor can count how
+     * many times has this program restarted. */
+    save_pid();
+
+    /* It takes some time for all services checked by this program to be
+     * responsive. We need to wait for them to be running before enabling WDT1,
+     * or WDT1 may time out too soon. */
+    wait_until_all_services_responsive();
+
+    if (enable_WDT1(&wdt1)) {
+        return -1;
+    }
+
+    /* Disable WDT2 to indicate successfully booting from primary flash. */
+    disable_WDT2();
+
+    /* Check services' aliveness every 5 seconds. Only when all services are
+     * alive should WDT1 be kicked. */
+    while (1) {
+        if (all_services_alive()) {
+            kick_WDT1(wdt1);
+        }
+        sleep(5);
+    }
+
+    // should never reach here
+    disable_WDT1(wdt1);
+
+    return 0;
 }
